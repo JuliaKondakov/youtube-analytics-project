571863cef191feaa1e2b64be1b3b92997b956bce
Julia Kondakov <juliakondakov@MacBook-Pro-Julia.fritz.box>
2023-03-29 21:30:28 +0200

    добавлен ключ API, установлен в переменную, раализован класс Channel, метод print_info

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,18 @@
+import os
+
+import isodate
+
+from helper.youtube_api_manual import youtube, printj
 
 
 class Channel:
+    api_key = os.getenv('YT_API_KEY')
     """Класс для ютуб-канала"""
-
-    def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+    def __init__(self, channel_id) -> None:
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel_id = 'UC1eFXmJNkjITxPFWTy6RsWg'  # Редакция
+        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        printj(channel)
